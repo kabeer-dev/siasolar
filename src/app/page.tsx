4fe18b0b78e5da41453f291c4c824deb1b34af53
kabeer-dev <kabeerahmed.dev@gmail.com>
2026-06-02 12:12:59 +0500

    ADD new coming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import HomePage from '@/components/pages/home';
-import {generateMetadata} from '@/utils/metadataUtils';
-import {SITE_METADATA} from '@/constants/general';
+import ComingSoon from '@/components/pages/comingSoon';
 
 export default function Home() {
-  return <HomePage />;
+  return <ComingSoon />;
 }
 
-export const metadata = async () =>
-  await generateMetadata({
-    pageName: SITE_METADATA.homePage.title,
-    description: SITE_METADATA.homePage.description,
-  });
+export const metadata = {
+  title: 'SIA Solar & Electric — Coming Soon',
+  description:
+    'SIA Solar specializes in commercial solar installations. Website launching soon.',
+};
